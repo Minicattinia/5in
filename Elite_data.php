a513b87133da9a5f6249d42fec1070c9a665ad49
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2021-03-09 11:41:04 +0700

    Add files via upload

---
+++ Elite_data.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+$useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Safari/537.36 Edg/89.0.774.45";
+
+
+
+$ssid_ltc = "PHPSESSID=nkbc3capl5ea443mocjqr5h210";
+
+$ssid_dgb = "PHPSESSID=3nmi81eq13ar8hbrdbjde5jav2";
+
+$ssid_trx = "PHPSESSID=uk094vr966d185qa17bi2ut9v2";
+
+$ssid_doge = "PHPSESSID=jgpjo5sgv2tr7kur6j3s4epmc0";
+
+$ssid_btc = "PHPSESSID=q1sa95upg189e33j4el6lprva4";
